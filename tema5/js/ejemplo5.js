88f5264480788c0cdf13360cf568f24d1f0b834c
nacho <ignacio.cubhij@educa.jcyl.es>
2023-11-08 21:05:49 +0100

    corregir errores

--- tema5/js/ejemplo5.js
+++ tema5/js/ejemplo5.js
@@ -2,31 +2,81 @@ document.addEventListener('DOMContentLoaded', iniciar, false);
 
 function iniciar() {
     document.getElementById("id_formulario").addEventListener('submit', validaForm);
-    document.getElementById("idCodPostal").addEventListener("keypress", e => {
-        if ((isNaN(e.key)) || (e.key == " ")) {
-            e.preventDefault()
-        }
+    document.getElementById('btnVal').addEventListener('click', function () {
+        console.log(document.getElementById('miSelect').options[document.getElementById('miSelect').selectedIndex].value);
+        console.log(document.getElementById('miSelect').options[document.getElementById('miSelect').selectedIndex].text);
     })
-    document.getElementById('idTerms').addEventListener('click', validaCheck);
 }
 
 function validaForm(evento) {
     evento.preventDefault();
-    let usuario = document.getElementById("usuario").value
-    if (usuario.length == 0) {
-        window.alert("No has escrito nada en el usuario")
-        return false
-    }
 
-    let clave = document.getElementById("clave").value
-    if (clave.length < 6) {
-        window.alert("La clave no es válida")
-        return false
-    }
+    validaUsu();
+    validaClave();
+    validaPostal();
+    validaCheck();
 
     this.submit();
 }
 
-function validaCheck() {
-    document.getElementById("submit").disabled=!document.getElementById("submit").disabled;
+function validaUsu() {
+    let usuario = document.getElementById("usuario").value
+    if (usuario.length == 0) {
+        window.alert("No has escrito nada en el usuario");
+    }
 }
+
+function validaClave() {
+    let clave = document.getElementById("clave").value
+    if (clave.length < 6) {
+        window.alert("La clave no es válida");
+    }
+}
+
+function validaPostal() {
+    document.getElementById("idCodPostal").addEventListener("keypress", e => {
+        if ((isNaN(e.key)) || (e.key == " ")) {
+            e.preventDefault();
+        }
+    })
+}
+
+function validaCheck() {
+    document.getElementById("submit").disabled = !document.getElementById("submit").disabled;
+    console.log(document.getElementById("submit").disabled);
+}
+
+function nueva() {
+    var siguiente = document.getElementById('miSelect').options.length + 1;
+    var opcion = new Option(`opcion${siguiente}`, `option${siguiente}`, false, false);
+    document.getElementById('miSelect').options[document.getElementById('miSelect').options.length] = opcion;
+}
+
+document.getElementById('newOpt').addEventListener('click', nueva);
+
+var select = document.getElementById('miSelect');
+function validarOpcion(opcion){
+    for (let i = 0; i < select.options.length; i++) {
+        const element = select.options[i].substring(0, select.value.length - 1);
+        
+        if(opcion.value != element){
+            return opcion.value;
+        }
+    }
+}
+
+function validarNumero(opcion){
+    for (let i = 0; i < select.options.length; i++) {
+        const element = select.options[i].substring(select.value.length - 1);
+        
+        if(opcion.value != element){
+            return select.options[opcion.value];
+        }
+    }
+}
+
+function borrarOpcion(){
+    select.remove(validarNumero);
+}
+
+document.getElementById('delOpt').addEventListener('click', borrarOpcion());
