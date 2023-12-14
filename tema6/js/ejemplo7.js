05381aa82ffe9f290d5d6a566d430c38b2a3f75b
Nacho <nachocubo01@gmail.com>
2023-12-14 10:54:28 +0100

    hecho

--- tema6/js/ejemplo7.js
+++ tema6/js/ejemplo7.js
@@ -1,27 +1,32 @@
 var campo = document.getElementById('campo');
 
-document.getElementsByTagName('button')[0].addEventListener('click', (e)=>{
+document.getElementsByTagName('button')[0].addEventListener('click', (e) => {
     e.preventDefault();
 
-    if(campo.value === ""){
-        alert('inserta algo eb el campo');
+    if (campo.value === "") {
+        alert('Inserta algo en el campo');
     } else {
         var line = document.createElement('li');
         line.textContent = campo.value;
         console.log(line.textContent);
         var lista = document.getElementsByTagName('ul')[0];
 
-        if(lista.length != 0){
-            for(let i = 0; i < lista.childNodes.length; i++){
-                if (lista.childNodes[i] != campo.value) {
-                    lista.appendChild(line);
+        if (lista.childNodes.length != 0) {
+            let valueExists = false;
+            for (let i = 0; i < lista.childNodes.length; i++) {
+                if (lista.childNodes[i].textContent === campo.value) {
+                    valueExists = true;
+                    break;
                 }
+            }
+
+            if (!valueExists) {
+                lista.appendChild(line);
             }
         } else {
             lista.appendChild(line);
         }
-
     }
 
     campo.value = "";
-})
+});
