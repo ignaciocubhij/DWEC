51388258c48a9c28cadb47f774b65ba3a0f18d71
nacho <ignacio.cubhij@educa.jcyl.es>
2024-01-22 17:33:10 +0100

    gdfg

--- tema8/ej4/js/ej4.js
+++ tema8/ej4/js/ej4.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-    $('a').each(function(i, elem){
-        if ($(elem).href.substr(-1)) {
-            
-        }
+    $('button').click(function(){
+        $('a').each(function(index, elem){
+            if ($(elem).attr('href').slice(-4) == '.com') {
+                $(elem).css('background-color', 'red');
+            }
+        })
     })
 })
