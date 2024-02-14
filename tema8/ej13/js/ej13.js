7ca7cab6692396243c8c36721be64733558740eb
nacho <ignacio.cubhij@educa.jcyl.es>
2024-02-14 21:20:19 +0100

    ejercicios de repaso

---
+++ tema8/ej13/js/ej13.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', finiciar);
+
+function clean(node) {
+	for (var n = 0; n < node.childNodes.length; n++) {
+		var child = node.childNodes[n];
+		if(child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+			node.removeChild(child);
+			n--;
+		} else if (child.nodeType === 1) {
+			clean(child);
+		}
+	}
+}
+
+function crear(e) {
+    var msg = document.createTextNode(e.target.getAttribute('data-tooltip'));
+    var msgBox = document.createElement('div');
+
+    msgBox.appendChild(msg);
+    msgBox.classList.add('tooltip');
+    e.target.appendChild(msgBox);
+}
+
+function destruir(e){
+    e.target.removeChild(e.target.firstChild);
+}
+
+function mover(e){
+    var tooltip = e.target.querySelector(':first-child');
+    console.log(e.target.value);
+    
+    tooltip.style.left = e.clientX + 20 + "px";
+    tooltip.style.top = e.clientY + 20 + "px";
+}
+
+function cambiar(e){
+    if (e.target.nextSibling !== null) {
+        e.target.parentNode.insertBefore(e.target.lastSibling);
+    }
+}
+
+function finiciar() {
+    clean(document.body);
+    var box = document.querySelectorAll('div');
+
+    box.forEach((elem, index) => {
+        elem.addEventListener('mouseover', crear);
+        elem.addEventListener('mouseout', destruir);
+        elem.addEventListener('mousemove', mover);
+        elem.addEventListener('click', cambiar);
+    })
+}
